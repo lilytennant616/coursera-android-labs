44c0eddb18cf979b7b41e50a5820121f1310f11f
lilytennant616 <67299441+lilytennant616@users.noreply.github.com>
2021-04-16 23:16:45 +0800

    complete todo work for lab6

--- Notifications/Lab6_NotificationsLab/src/course/labs/notificationslab/DownloaderTaskFragment.java
+++ Notifications/Lab6_NotificationsLab/src/course/labs/notificationslab/DownloaderTaskFragment.java
@@ -15,6 +15,7 @@ import android.app.Fragment;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -41,17 +42,16 @@ public class DownloaderTaskFragment extends Fragment {
 		setRetainInstance(true);
 
 		// TODO: Create new DownloaderTask that "downloads" data
-
+		DownloaderTask task=new DownloaderTask();
 		
 		// TODO: Retrieve arguments from DownloaderTaskFragment
 		// Prepare them for use with DownloaderTask.
+		ArrayList<Integer> resourceIDList=getArguments().getIntegerArrayList(MainActivity.TAG_FRIEND_RES_IDS);
+		Integer[] resourceIDs= new Integer[resourceIDList.size()];
+		resourceIDList.toArray(resourceIDs);
 
-
-		
-		
-		
 		// TODO: Start the DownloaderTask
-
+		task.execute(resourceIDs);
 		
 	}
 
@@ -85,200 +85,185 @@ public class DownloaderTaskFragment extends Fragment {
 	// out). Ultimately, it must also pass newly available data back to
 	// the hosting Activity using the DownloadFinishedListener interface.
 
-	// public class DownloaderTask extends ...
+	 public class DownloaderTask extends AsyncTask<Integer,Void,String[]> {
+
+		 @Override
+		 protected String[] doInBackground(Integer... resourceIDS) {
+			 return downloadTweets(resourceIDS);
+		 }
+
+		 @Override
+		 protected void onPostExecute(String[] result) {
+			 MainActivity main = (MainActivity)getActivity();
+			 if (main!=null)
+			     main.notifyDataRefreshed(result);
+		 }
+
+		 // TODO: Uncomment this helper method
+		 // Simulates downloading Twitter data from the network
+		 private String[] downloadTweets(Integer resourceIDS[]) {
+
+			 final int simulatedDelay = 2000;
+			 String[] feeds = new String[resourceIDS.length];
+			 boolean downLoadCompleted = false;
+
+			 try {
+				 for (int idx = 0; idx < resourceIDS.length; idx++) {
+					 InputStream inputStream;
+					 BufferedReader in;
+					 try {
+						 // Pretend downloading takes a long time
+						 Thread.sleep(simulatedDelay);
+					 } catch (InterruptedException e) {
+						 e.printStackTrace();
+					 }
+
+					 inputStream = mContext.getResources().openRawResource(
+							 resourceIDS[idx]);
+					 in = new BufferedReader(new InputStreamReader(inputStream));
+
+					 String readLine;
+					 StringBuffer buf = new StringBuffer();
+
+					 while ((readLine = in.readLine()) != null) {
+						 buf.append(readLine);
+					 }
+
+					 feeds[idx] = buf.toString();
+
+					 if (null != in) {
+						 in.close();
+					 }
+				 }
+
+				 downLoadCompleted = true;
+				 saveTweetsToFile(feeds);
+
+			 } catch (IOException e) {
+				 e.printStackTrace();
+			 }
+
+			 // Notify user that downloading has finished
+			 notify(downLoadCompleted);
+
+			 return feeds;
+
+		 }
+
+		 // Uncomment this helper method.
+		 // If necessary, notifies the user that the tweet downloads are
+		 // complete. Sends an ordered broadcast back to the BroadcastReceiver in
+		 // MainActivity to determine whether the notification is necessary.
 
 
-	
-	
-	
-	
-	
-	
-		// TODO: Uncomment this helper method
-		// Simulates downloading Twitter data from the network
+		 private void notify(final boolean success) {
 
-/* 
-	 
-	  private String[] downloadTweets(Integer resourceIDS[]) {
-	 
-			final int simulatedDelay = 2000;
-			String[] feeds = new String[resourceIDS.length];
-			boolean downLoadCompleted = false;
+			 final Intent restartMainActivityIntent = new Intent(mContext,
+					 MainActivity.class);
+			 restartMainActivityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-			try {
-				for (int idx = 0; idx < resourceIDS.length; idx++) {
-					InputStream inputStream;
-					BufferedReader in;
-					try {
-						// Pretend downloading takes a long time
-						Thread.sleep(simulatedDelay);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
+			 // Sends an ordered broadcast to determine whether MainActivity is
+			 // active and in the foreground. Creates a new BroadcastReceiver
+			 // to receive a result indicating the state of MainActivity
 
-					inputStream = mContext.getResources().openRawResource(
-							resourceIDS[idx]);
-					in = new BufferedReader(new InputStreamReader(inputStream));
+			 // The Action for this broadcast Intent is
+			 // MainActivity.DATA_REFRESHED_ACTION
+			 // The result, MainActivity.IS_ALIVE, indicates that MainActivity is
+			 // active and in the foreground.
 
-					String readLine;
-					StringBuffer buf = new StringBuffer();
+			 mContext.sendOrderedBroadcast(new Intent(
+							 MainActivity.DATA_REFRESHED_ACTION), null,
+					 new BroadcastReceiver() {
 
-					while ((readLine = in.readLine()) != null) {
-						buf.append(readLine);
-					}
+						 final String failMsg = mContext
+								 .getString(R.string.download_failed_string);
+						 final String successMsg = mContext
+								 .getString(R.string.download_succes_string);
+						 final String notificationSentMsg = mContext
+								 .getString(R.string.notification_sent_string);
 
-					feeds[idx] = buf.toString();
+						 @Override
+						 public void onReceive(Context context, Intent intent) {
 
-					if (null != in) {
-						in.close();
-					}
-				}
+							 // TODO: Check whether or not the MainActivity
+							 // received the broadcast
 
-				downLoadCompleted = true;
-				saveTweetsToFile(feeds);
+							 if (getResultCode()!=MainActivity.IS_ALIVE) {
 
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+								 // TODO: If not, create a PendingIntent using
+								 // the
+								 // restartMainActivityIntent and set its flags
+								 // to FLAG_UPDATE_CURRENT
+								PendingIntent contentIntent=PendingIntent.getActivity(mContext,0,restartMainActivityIntent,PendingIntent.FLAG_UPDATE_CURRENT);
 
-			// Notify user that downloading has finished
-			notify(downLoadCompleted);
+								 // Uses R.layout.custom_notification for the
+								 // layout of the notification View. The xml
+								 // file is in res/layout/custom_notification.xml
 
-			return feeds;
+								 RemoteViews mContentView = new RemoteViews(
+										 mContext.getPackageName(),
+										 R.layout.custom_notification);
 
-		}
-*/
-		// Uncomment this helper method.
-		// If necessary, notifies the user that the tweet downloads are
-		// complete. Sends an ordered broadcast back to the BroadcastReceiver in
-		// MainActivity to determine whether the notification is necessary.
+								 // TODO: Set the notification View's text to
+								 // reflect whether the download completed
+								 // successfully
+								 String resultText=success?successMsg:failMsg;
+								 mContentView.setTextViewText(R.id.text,resultText);
 
-	/*
-		private void notify(final boolean success) {
+								 // TODO: Use the Notification.Builder class to
+								 // create the Notification. You will have to set
+								 // several pieces of information. You can use
+								 // android.R.drawable.stat_sys_warning
+								 // for the small icon. You should also
+								 // setAutoCancel(true).
 
-			final Intent restartMainActivityIntent = new Intent(mContext,
-					MainActivity.class);
-			restartMainActivityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+								 Notification.Builder notificationBuilder = new Notification.Builder(mContext)
+										 //.setTicker(resultText)
+										 .setSmallIcon(android.R.drawable.stat_sys_warning)
+										 .setAutoCancel(true)
+										 .setContentIntent(contentIntent)
+										 .setContent(mContentView);
 
-			// Sends an ordered broadcast to determine whether MainActivity is
-			// active and in the foreground. Creates a new BroadcastReceiver
-			// to receive a result indicating the state of MainActivity
+								 // TODO: Send the notification
+								 NotificationManager mNotificationManager=(NotificationManager)(mContext.getSystemService(Context.NOTIFICATION_SERVICE));
+								 mNotificationManager.notify(MY_NOTIFICATION_ID,notificationBuilder.build());
 
-			// The Action for this broadcast Intent is
-			// MainActivity.DATA_REFRESHED_ACTION
-			// The result, MainActivity.IS_ALIVE, indicates that MainActivity is
-			// active and in the foreground.
+								 Toast.makeText(mContext, notificationSentMsg,
+										 Toast.LENGTH_LONG).show();
 
-			mContext.sendOrderedBroadcast(new Intent(
-					MainActivity.DATA_REFRESHED_ACTION), null,
-					new BroadcastReceiver() {
-
-						final String failMsg = mContext
-								.getString(R.string.download_failed_string);
-						final String successMsg = mContext
-								.getString(R.string.download_succes_string);
-						final String notificationSentMsg = mContext
-								.getString(R.string.notification_sent_string);
-
-						@Override
-						public void onReceive(Context context, Intent intent) {
-
-							// TODO: Check whether or not the MainActivity
-							// received the broadcast
-
-							if (true || false) {
-
-								// TODO: If not, create a PendingIntent using
-								// the
-								// restartMainActivityIntent and set its flags
-								// to FLAG_UPDATE_CURRENT
+							 } else {
+								 Toast.makeText(mContext,
+										 success ? successMsg : failMsg,
+										 Toast.LENGTH_LONG).show();
+							 }
+						 }
+					 }, null, 0, null, null);
+		 }
 
 
+		 // Uncomment this helper method
+		 // Saves the tweets to a file
 
 
+		 private void saveTweetsToFile(String[] result) {
+			 PrintWriter writer = null;
+			 try {
+				 FileOutputStream fos = mContext.openFileOutput(
+						 MainActivity.TWEET_FILENAME, Context.MODE_PRIVATE);
+				 writer = new PrintWriter(new BufferedWriter(
+						 new OutputStreamWriter(fos)));
 
-
-
-
-								// Uses R.layout.custom_notification for the
-								// layout of the notification View. The xml
-								// file is in res/layout/custom_notification.xml
-
-								RemoteViews mContentView = new RemoteViews(
-										mContext.getPackageName(),
-										R.layout.custom_notification);
-
-								// TODO: Set the notification View's text to
-								// reflect whether the download completed
-								// successfully
-
-
-
-
-								// TODO: Use the Notification.Builder class to
-								// create the Notification. You will have to set
-								// several pieces of information. You can use
-								// android.R.drawable.stat_sys_warning
-								// for the small icon. You should also
-								// setAutoCancel(true).
-
-								Notification.Builder notificationBuilder = null;
-
-								// TODO: Send the notification
-
-
-
-
-
-
-								Toast.makeText(mContext, notificationSentMsg,
-										Toast.LENGTH_LONG).show();
-
-							} else {
-								Toast.makeText(mContext,
-										success ? successMsg : failMsg,
-										Toast.LENGTH_LONG).show();
-							}
-						}
-					}, null, 0, null, null);
-		}
-
-*/
-	
-		// Uncomment this helper method
-		// Saves the tweets to a file
-	
-/*	
-		private void saveTweetsToFile(String[] result) {
-			PrintWriter writer = null;
-			try {
-				FileOutputStream fos = mContext.openFileOutput(
-						MainActivity.TWEET_FILENAME, Context.MODE_PRIVATE);
-				writer = new PrintWriter(new BufferedWriter(
-						new OutputStreamWriter(fos)));
-
-				for (String s : result) {
-					writer.println(s);
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-				if (null != writer) {
-					writer.close();
-				}
-			}
-		}
-*/
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
+				 for (String s : result) {
+					 writer.println(s);
+				 }
+			 } catch (IOException e) {
+				 e.printStackTrace();
+			 } finally {
+				 if (null != writer) {
+					 writer.close();
+				 }
+			 }
+		 }
+	 }
 	
 }
